2c84daefdcf1569c57686ab5004b1466bebfae60
jstiff <jwstiff8@gmail.com>
2020-08-04 12:44:03 -0500

    lesson text from store with backspace works

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,7 +1,8 @@
-import { combineReducers } from 'redux';
-import errors from './errorsReducer';
-import loginMode from './loginModeReducer';
-import user from './userReducer';
+import { combineReducers } from "redux";
+import errors from "./errorsReducer";
+import loginMode from "./loginModeReducer";
+import user from "./userReducer";
+import lessonText from "./lessonText";
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
@@ -12,7 +13,8 @@ import user from './userReducer';
 const rootReducer = combineReducers({
   errors, // contains registrationMessage and loginMessage
   loginMode, // will have a value of 'login' or 'registration' to control which screen is shown
-  user, // will have an id and username if someone is logged in
+  user,
+  lessonText, // will have an id and username if someone is logged in
 });
 
 export default rootReducer;
---
+++ src/redux/reducers/lessonText.js
@@ -0,0 +1,27 @@
+const lessonText = () => {
+  return [
+    "t",
+    "h",
+    "i",
+    "s",
+    " ",
+    "i",
+    "s",
+    " ",
+    "p",
+    "r",
+    "a",
+    "c",
+    "t",
+    "i",
+    "c",
+    "e",
+    " ",
+    "t",
+    "e",
+    "x",
+    "t",
+  ];
+};
+
+export default lessonText;
