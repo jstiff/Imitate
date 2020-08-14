67e9dc1650b9f32956aa6392081b4c274bab8c33
jstiff <jwstiff8@gmail.com>
2020-08-14 09:43:18 -0500

    changed sql query for historyPage

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -11,7 +11,15 @@ const router = express.Router();
 
 router.get("/history", rejectUnauthenticated, (req, res) => {
   console.log("inside HISTORY route", req.user);
-  const queryString = `SELECT "percent_correct", "time_stamp" FROM "metrics" WHERE "user_id"=${req.user.id};`;
+  const queryString = `
+  SELECT  "metrics"."id" AS "metrics_id", "avatar_url", "name", "repo_name", "file_name","time_stamp", "percent_correct", "comments"  FROM "metrics"
+  FULL JOIN "chosen_file" ON "metrics"."file_id" = "chosen_file"."id"
+  FULL JOIN "repos" ON "chosen_file"."repo_id" = "repos"."id"
+  FULL JOIN "fav_coders" ON "repos"."repo_owner" = "fav_coders"."id"
+  FULL JOIN "user_favCoder" ON "user_favCoder"."fav_coder_id" = "fav_coders"."id"
+  FULL JOIN "user" ON "user_favCoder"."user_id" = "user"."id"
+  WHERE "user"."id" = ${req.user.id}
+  ;`;
   pool
     .query(queryString)
     .then((result) => {
@@ -50,6 +58,7 @@ router.post("/register", (req, res, next) => {
 router.post("/results", rejectUnauthenticated, async (req, res, next) => {
   const time = moment().format("LLLL");
   const fav_coder = req.body[0];
+  const lessonUserId = req.user.id;
   const repos = req.body[1];
   const chosen_file = req.body[2];
   const percent_correct = req.body[3];
@@ -59,6 +68,8 @@ router.post("/results", rejectUnauthenticated, async (req, res, next) => {
   try {
     console.log("results", req.body);
     const fav_coder_query = `INSERT INTO fav_coders (name, user_name, avatar_url) VALUES ($1, $2, $3) RETURNING id;`;
+
+    const joinTableQuery = `INSERT INTO  "user_favCoder" (user_id, fav_coder_id) VALUES ($1,$2);`;
 
     const repo_query = `INSERT INTO repos (repo_name, repo_url, repo_owner) VALUES ($1, $2, $3) RETURNING id;`;
 
@@ -72,6 +83,7 @@ router.post("/results", rejectUnauthenticated, async (req, res, next) => {
       fav_coder.avatar_url,
     ]);
     const fav_coderId = fav_coderInstance.rows[0].id;
+    await connection.query(joinTableQuery, [lessonUserId, fav_coderId]);
     console.log("Fav_coder", fav_coderId, fav_coderInstance.rows[0].id);
     const repoInstance = await connection.query(repo_query, [
       repos.repo_name,
