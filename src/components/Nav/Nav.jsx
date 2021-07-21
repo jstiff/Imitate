1485ba66d081981f38c204e0449b360b1794e14d
jstiff <jwstiff8@gmail.com>
2021-07-21 10:37:39 -0500

    refactored to functional component

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import LogOutButton from "../LogOutButton/LogOutButton";
+import "./Nav.css";
+
+
+const Nav = () => {
+	const user = useSelector(state => state.user);
+	const dispatch = useDispatch();
+
+	return (
+		<div className="nav">
+		  <Link to="/home">
+		    <h1 className="nav-title">Imitate Hooks</h1>
+		  </Link>
+	      
+		  <div className="nav-right">
+		    <h4 className="welcome">Welcome, {user.first_name}!</h4>
+	      
+		    <Link
+		      onClick={() => dispatch({ type: "CLEAR_ON_LOGOUT" })}
+		      className="nav-link"
+		      to="/home"
+		    >
+		     
+		      {user.id ? "Home" : "Login / Register"}
+		    </Link>
+		    
+		    {user.id && (
+		      <>
+			<Link className="nav-link" to="/lesson">
+			  Lesson Page
+			</Link>
+			<LogOutButton />
+		      </>
+		    )}
+		   
+		    <Link className="nav-link" to="/about">
+		      About
+		    </Link>
+		  </div>
+		</div>
+	      );
+}
+
+
+
+
+
+export default Nav;
