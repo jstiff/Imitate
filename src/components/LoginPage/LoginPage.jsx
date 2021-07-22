764f13f3d7a8d09628e499dc2d09e42e6d64e8eb
jstiff <jwstiff8@gmail.com>
2021-07-21 21:55:49 -0500

    refactored to function

---
+++ src/components/LoginPage/LoginPage.jsx
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+
+const LoginPage = () => {
+	const errors = useSelector(state => state.errors);
+	const dispatch = useDispatch();
+	const [login, setLogin] = useState({
+		username: "",
+		password: "",
+	      }); 
+
+	const loginUser = (event) => {
+		event.preventDefault();
+	    
+		if (login.username && login.password) {
+		  dispatch({
+		    type: "LOGIN",
+		    payload: {
+		      username: login.username,
+		      password: login.password,
+		    },
+		  });
+		} else {
+		  dispatch({ type: "LOGIN_INPUT_ERROR" });
+		}
+	      }; // end login
+	    
+	const handleInputChangeFor = (propertyName) => (event) => {
+		setLogin((prevState)=>({
+			...prevState,
+		  [propertyName]: event.target.value,
+		}));
+	      };
+
+	      return (
+		<div>
+		  {errors.loginMessage && (
+		    <h2 className="alert" role="alert">
+		      {errors.loginMessage}
+		    </h2>
+		  )}
+		  <div className="container">
+		    <form onSubmit={loginUser}>
+		      <h1>Login</h1>
+		      <div className="register-form">
+			<label htmlFor="username">
+			  Username:
+			  <input
+			    type="text"
+			    name="username"
+			    value={login.username}
+			    onChange={handleInputChangeFor("username")}
+			  />
+			</label>
+		      </div>
+		      <div className="register-form">
+			<label htmlFor="password">
+			  Password:
+			  <input
+			    type="password"
+			    name="password"
+			    value={login.password}
+			    onChange={handleInputChangeFor("password")}
+			  />
+			</label>
+		      </div>
+		      <div>
+			<input
+			  className="register-form-button button-ghost"
+			  type="submit"
+			  name="submit"
+			  value="Log In"
+			/>
+		      </div>
+		    </form>
+		  </div>
+		  <center>
+		    <button
+		      type="button"
+		      className="link-button"
+		      onClick={() => {
+			dispatch({ type: "SET_TO_REGISTER_MODE" });
+		      }}
+		    >
+		      Register
+		    </button>
+		  </center>
+		</div>
+	      );
+}
+
+
+
+export default LoginPage;
