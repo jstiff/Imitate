0f9208bb79f91bb8e0cb488624203a6b7a780861
jstiff <jwstiff8@gmail.com>
2021-07-29 16:06:24 -0500

    adding github oAuth

---
+++ server/strategies/github.strategy.js
@@ -0,0 +1,38 @@
+
+const passport = require('passport');
+const GitHubStrategy = require('passport-github2').Strategy;
+
+
+
+
+
+
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.  However, since this example does not
+//   have a database of user records, the complete GitHub profile is serialized
+//   and deserialized.
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+
+passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: "http://127.0.0.1:3000/auth/github/callback"
+  },
+  (accessToken, refreshToken, profile, done) => {
+    console.log("GITHUB OATH", profile);
+    process.nextTick(function () {
+      
+      return done(null, profile);
+    });
+  }
+));
